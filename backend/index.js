d6aa297106f9124bdbf40e9cf5f8e4ecc00f694d
Gutu <gutujirex@gmail.com>
2025-09-29 00:51:13 +0300

    index.js * issue5

--- backend/index.js
+++ backend/index.js
@@ -40,8 +40,8 @@ app.use("/api/todo", todoRouter);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-  // Express 5.x: use '/:path*' for catch-all
-  app.get("/:path*", (req, res) => {
+  // Express 5.x: use '/*' for catch-all
+  app.get("/*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
   });
 }
