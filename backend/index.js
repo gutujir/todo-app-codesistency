99de65213d8d941d6c469051ec3d48d78553e896
Gutu <gutujirex@gmail.com>
2025-09-29 01:13:48 +0300

    index.js * issue7

--- backend/index.js
+++ backend/index.js
@@ -38,9 +38,10 @@ app.use("/api/auth", authRoutes);
 app.use("/api/todo", todoRouter);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.use(express.static(path.join(__dirname, "frontend", "dist")));
 
-  app.get("*", (req, res) => {
+  // Express 5 requires '/*' instead of '*'
+  app.get("/*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
   });
 }
