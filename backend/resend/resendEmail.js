b9409bf0464b11cc3a49f42c24ae11767d3b102d
Gutu <gutujirex@gmail.com>
2025-09-28 22:38:08 +0300

    some feature added on login and register

--- backend/resend/resendEmail.js
+++ backend/resend/resendEmail.js
@@ -7,7 +7,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export const sendEmail = async ({ to, subject, html }) => {
   try {
     const data = await resend.emails.send({
-      from: "Your Name <onboarding@resend.dev>",
+      from: "TodoPro <onboarding@resend.dev>",
       to,
       subject,
       html,
